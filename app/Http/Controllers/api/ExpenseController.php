3e704d7679653387e3d60ff2c95dc85464ab95f1
Shamar Stewart <jayhacker19@gmail.com>
2022-03-23 10:49:04 -0500

    expense api firt commit

---
+++ app/Http/Controllers/api/ExpenseController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Expense;
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+    public function index()
+    {
+        $expenses = Expense::all();
+
+        return response()->json($expenses);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'amount' => 'required',
+            'description' => 'required' //optional if you want this to be required
+        ]);
+        $expense = Expense::create($request->all());
+        return response()->json(['message'=> 'expense created',
+        'expense' => $expense]);
+    }
+
+    public function show(Expense $expense)
+    {
+
+        return response()->json($expense);
+    }
+
+    public function update(Request $request, Expense $expense)
+    {
+        $request->validate([
+            'name' => 'required',
+            'amount' => 'required',
+            'description' => 'required' //optional if you want this to be required
+        ]);
+        $expense->name = $request->name();
+        $expense->amount = $request->amount();
+        $expense->description = $request->description();
+        $expense->save();
+
+        return response()->json([
+            'message' => 'expense updated!',
+            'expense' => $expense
+        ]);
+    }
+
+    public function destroy(Expense $expense)
+    {
+        $expense->delete();
+        return response()->json([
+            'message' => 'expense deleted'
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\api\ExpenseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.all');
+Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+Route::get('/expenses/show/{expense}', [ExpenseController::class, 'show'])->name('expenses.show');
+Route::put('/expenses/update/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+Route::delete('/expenses/delete/{expense}', [ExpenseController::class, 'destory'])->name('expenses.destroy');
